18daa4cc144ad427981464bd2367f29cc5c1b9bf
Nguyen Quoc Huy <nqh.webdev@gmail.com>
2020-09-12 19:24:41 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,16 +2,14 @@ const router = require('express').Router();
 
 const authRouter = require('./auth');
 const accountRouter = require('./accounts');
-const coursesRouter = require('./courses');
-const forumRouter = require('./forum');
-const libraryRouter = require('./library');
-const indexRouter = require('./main');
 
 router.use('/auth', authRouter);
 router.use('/account', accountRouter);
-router.use('/courses', coursesRouter);
-router.use('/forum', forumRouter);
-router.use('/library', libraryRouter);
-router.use('/', indexRouter);
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    mess: req.flash('mess'),
+  });
+});
 
 module.exports = router;
